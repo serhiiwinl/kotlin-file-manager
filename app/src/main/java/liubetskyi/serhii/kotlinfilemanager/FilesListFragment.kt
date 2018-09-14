02c127d78c66c4d71cf34516bc24ca049130bcc4
SLiubetskyi <Serhii.Liubetskyi@onseo.biz>
2018-09-14 15:30:38 +0300

    added correct drawable resources and Builder

--- app/src/main/java/liubetskyi/serhii/kotlinfilemanager/FilesListFragment.kt
+++ app/src/main/java/liubetskyi/serhii/kotlinfilemanager/FilesListFragment.kt
@@ -10,9 +10,23 @@ class FilesListFragment : Fragment() {
 
     companion object {
         private const val ARG_PATH: String = "liubetskyi.serhii.kotlinfilemanager.fileslist.path"
+        fun build(block: Builder.() -> Unit) { Builder().apply(block).build() }
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return super.onCreateView(inflater, container, savedInstanceState)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_files_list, container, false)
+    }
+
+    class Builder {
+        var path: String = ""
+
+        fun build(): FilesListFragment {
+            val fragment = FilesListFragment()
+            val args = Bundle()
+            args.putString(ARG_PATH, path)
+            fragment.arguments = args;
+            return fragment
+        }
     }
 }
